836e94b746c4615adb10754eecf07e5fd9d01460
FarzanAkhtar1 <47488690+FarzanAkhtar1@users.noreply.github.com>
2021-05-10 00:39:34 +0100

    Update main.py
    Handles user inptu

--- main.py
+++ main.py
@@ -19,8 +19,18 @@ async def on_ready():
     print(
         f'{client.user} has connected to: '
         f'{guild.name} (id: {guild.id}) '
-        f'{client.user} kappa123'
+        f'{client.user} kappa1234'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    greeting = "Hello"
+    input = message.content
+    if "hello" in input.lower():
+        response = greeting
+        await message.channel.send(response)
 
 client.run(TOKEN)
 
