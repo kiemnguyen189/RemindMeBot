de7c98e549f778705923ffe4ce57312d3d0e6e62
Kiem Nguyen <kiemnguyen189@gmail.com>
2021-05-11 21:53:55 +0100

    Small RegEx changes (Still incomplete)

--- main.py
+++ main.py
@@ -35,20 +35,29 @@ async def on_message(message):
     #     await message.channel.send(response)
     # TODO: RegEx for inputs.
     if input.lower().startswith('/h'):
-        dateMatch = re.search(r'\d{2}-\d{2}-\d{4}', input)
-        timeMatch = re.search(r'\d{2}:\d{2}', input)
-        # Match the date
-        if dateMatch != None:
-            date = datetime.strptime(dateMatch.group(), '%d-%m-%Y')
-            dateStr = date.strftime('%d/%m/%Y')
-            greeting += " The date is: " + dateStr
-        # Match the time
-        if timeMatch != None:
-            time = datetime.strptime(timeMatch.group(), '%H:%M')
-            timeStr = time.strftime('%H:%M')
-            greeting += " The time is: " + timeStr
+        greeting += parseInput(input)   # ? Function to perform the RegEx
+        # TODO: Function to store parsed input
+        # TODO: Check if input date and time is valid (datetime in the future, not a past datetime)
         await message.channel.send(greeting)
 
+# Parses the user input in the text channel
+# * Returns: A default output string response from the bot.
+def parseInput(input):
+    output = ""
+    dateMatch = re.search(r'\d{2}-\d{2}-\d{4}', input)
+    timeMatch = re.search(r'\d{2}:\d{2}', input)
+    # Match the date
+    if dateMatch != None:
+        date = datetime.strptime(dateMatch.group(), '%d-%m-%Y')
+        dateStr = date.strftime('%d/%m/%Y')
+        output += " The date is: " + dateStr
+    # Match the time
+    if timeMatch != None:
+        time = datetime.strptime(timeMatch.group(), '%H:%M')
+        timeStr = time.strftime('%H:%M')
+        output += " The time is: " + timeStr
+    # TODO: Store the user note
+    return output
 
 client.run(TOKEN)
 
