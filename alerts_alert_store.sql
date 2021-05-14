def800ec6999f9a841e3907c68c2a5cdc00b7e1c
FarzanAkhtar1 <47488690+FarzanAkhtar1@users.noreply.github.com>
2021-05-14 18:15:22 +0100

    Update alerts_alert_store.sql

--- alerts_alert_store.sql
+++ alerts_alert_store.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `alerts` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `alerts`;
 -- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
 -- Host: localhost    Database: alerts
@@ -26,12 +24,16 @@ DROP TABLE IF EXISTS `alert_store`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alert_store` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `dates` date DEFAULT NULL,
-  `times` time DEFAULT NULL,
+  `date_notify` date DEFAULT NULL,
+  `time_notify` time DEFAULT NULL,
+  `date_created` date DEFAULT NULL,
+  `time_created` time DEFAULT NULL,
+  `userID` varchar(256) DEFAULT NULL,
+  `delta` varchar(256) DEFAULT NULL,
   `note` varchar(256) DEFAULT NULL,
   `sent` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +42,7 @@ CREATE TABLE `alert_store` (
 
 LOCK TABLES `alert_store` WRITE;
 /*!40000 ALTER TABLE `alert_store` DISABLE KEYS */;
-INSERT INTO `alert_store` VALUES (3,'2000-10-10','18:30:00','Fat chungus',1),(4,'2000-10-10','18:30:00','Fat chungus',1),(5,'2020-02-11','18:30:00','Fat chungus',1),(6,'2000-01-01','18:19:00','testign123',1),(7,'2000-01-01','18:19:00','testign123',1),(8,'2000-01-01','18:19:00','testign123',1),(10,'2020-05-14','18:19:00','testign123',1),(11,'2020-05-25','18:19:00','testign123',1),(12,'2021-05-25','18:19:00','testign123',0),(13,'2021-05-14','09:37:00','I hope this works',1),(14,'2021-05-14','09:50:00','Yeet',1);
+INSERT INTO `alert_store` VALUES (16,'2021-05-14','17:40:00','2021-05-14','17:11:40','135080434773131264','0:28:19.869301','mememem',1),(17,'2021-05-14','17:19:00','2021-05-14','17:17:12','135080434773131264','0:01:48.159923','LETS GOOOOO',1),(18,'2021-05-14','17:32:00','2021-05-14','17:31:33','135080434773131264','0:00:27.154392','LETS GOOOOO',1),(19,'2021-05-14','17:36:00','2021-05-14','17:35:10','135080434773131264','0:00:49.867163','LET\'S GOOOOO',1),(20,'2021-05-14','17:37:00','2021-05-14','17:36:28','135080434773131264','0:00:32.305256','lemon gang rise up ?',1),(21,'2021-05-14','17:38:00','2021-05-14','17:37:56','135080434773131264','0:00:04.313735','ඞ A M O G U S ඞ',1),(22,'2021-05-14','17:42:00','2021-05-14','17:41:24','134670639784132608','0:00:36.284331','Hi',1),(23,'2021-05-14','17:43:00','2021-05-14','17:42:15','135080434773131264','0:00:44.643330','<@!134670639784132608>',1),(24,'2021-05-14','17:47:00','2021-05-14','17:46:25','135080434773131264','0:00:34.932945','doiasdhsai0odas',1);
 /*!40000 ALTER TABLE `alert_store` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-14  9:53:33
+-- Dump completed on 2021-05-14 18:14:57
